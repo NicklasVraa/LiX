591279ad6cd4a2dbb2933a57dd8ea5f5a7beb18c
NicklasVraa <n.k.vraa@gmail.com>
2024-01-06 18:24:29 +0100

    Added standalone scripts.

---
+++ scripts/encrypt.sh
@@ -0,0 +1,1 @@
+qpdf --encrypt cL8M1d4619vW yT578K2TLc05 256 --modify=none --extract=n --print=none -- main.pdf encrypted.pdf
---
+++ scripts/impose.sh
@@ -0,0 +1,1 @@
+pdfjam --nup 2x1 --signature 12 --frame false --trim '0cm 0cm 0cm 0cm' --clip true --quiet --keepinfo --papersize '{12in,9in}' --tidy --longedge --outfile sheets.pdf main.pdf
