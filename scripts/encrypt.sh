70f62f7d4c181c128b21b7523f358594ce8491f0
NicklasVraa <n.k.vraa@gmail.com>
2024-01-07 12:53:17 +0100

    Updated scripts

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,1 +1,1 @@
-qpdf --encrypt cL8M1d4619vW yT578K2TLc05 256 --modify=none --extract=n --print=none -- main.pdf encrypted.pdf
+qpdf --encrypt userpass ownerpass 256 --modify=none --extract=n --print=none -- main.pdf encrypted.pdf
